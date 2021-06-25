3ee560b6c6ef051326354f4a22227c6c9dd0ab3b
Bartosz Daniluk <bartek.daniluk09@gmail.com>
2021-06-25 15:40:03 +0200

    update
    do dodania

---
+++ scrolling.js
@@ -0,0 +1,17 @@
+const navSlide =() =>{
+    const mobile_menu = document.querySelector('.mobile_menu');
+    const nav = document.querySelector('.nav_links');
+    const navlinks = document.querySelectorAll('.nav_links li');
+    mobile_menu.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+    });
+    navlinks.forEach((link,index)=>{
+        if(link.style.animation){
+            link.style.animation = '';
+        }
+        else{
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index /7+0.3}s`;
+        }
+    })
+}
+navSlide();
