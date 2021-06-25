0326f18c98dc593be2f55197093b618f10797e59
Bartosz Daniluk <bartek.daniluk09@gmail.com>
2021-06-25 16:55:59 +0200

    poprawa czytelności i dodanie animacji projektów

--- scrolling.js
+++ scrolling.js
@@ -1,16 +1,16 @@
-const navSlide =() =>{
+const navSlide = () => {
     const mobile_menu = document.querySelector('.mobile_menu');
     const nav = document.querySelector('.nav_links');
     const navlinks = document.querySelectorAll('.nav_links li');
-    mobile_menu.addEventListener('click',()=>{
+    mobile_menu.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
     });
-    navlinks.forEach((link,index)=>{
-        if(link.style.animation){
+    navlinks.forEach((link, index) => {
+        if (link.style.animation) {
             link.style.animation = '';
         }
-        else{
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index /7+0.3}s`;
+        else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
         }
     })
 }
