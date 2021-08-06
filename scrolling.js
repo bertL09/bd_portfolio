f0261b0affe33258f8be0784d06bcca569488896
Bartosz Daniluk <bartek.daniluk09@gmail.com>
2021-08-06 23:52:31 +0200

    finalne zmiany

--- scrolling.js
+++ scrolling.js
@@ -1,48 +1,10 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
-
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-}
-
-
-
 const navSlide = () => {
     const mobile_menu = document.querySelector('.mobile_menu');
     const nav = document.querySelector('.nav_links');
     const navlinks = document.querySelectorAll('.nav_links li');
     mobile_menu.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+        
     });
     navlinks.forEach((link, index) => {
         if (link.style.animation) {
@@ -54,3 +16,4 @@ const navSlide = () => {
     })
 }
 navSlide();
+
